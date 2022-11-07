7989fc174cfa2d24ee72dc8779b23d7e18b66c38
Maxi <maximilian.lincks@gmx.net>
2022-11-07 19:30:51 +0100

    added Kotlin Runtime

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
                  [org.clojure/clojure "1.11.1"]
-                 [de.fraunhofer.aisec/cpg-core "5.0.0"]
-                 [de.fraunhofer.aisec/cpg-analysis "5.0.0"]
+                 [de.fraunhofer.aisec/cpg-core "5.1.0"]
+                 [de.fraunhofer.aisec/cpg-analysis "5.1.0"]
+                 [org.jetbrains.kotlin/kotlin-runtime "1.2.71"]
                  ]
   :repl-options {:init-ns cpg-test.core}
   :resource-paths ["local-repo/core-7.2.100.202105180159.jar"]
