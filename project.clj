ca6a2ce256f92d5e56f4c9b1077b5471708c43e9
Maxi <maximilian.lincks@gmx.net>
2022-11-07 22:36:56 +0100

    added kotlin module

--- project.clj
+++ project.clj
@@ -7,8 +7,10 @@
                  [org.clojure/clojure "1.11.1"]
                  [de.fraunhofer.aisec/cpg-core "5.1.0"]
                  [de.fraunhofer.aisec/cpg-analysis "5.1.0"]
-                 [org.jetbrains.kotlin/kotlin-runtime "1.2.71"]
                  ]
   :repl-options {:init-ns cpg-test.core}
-  :resource-paths ["local-repo/core-7.2.100.202105180159.jar"]
+  :resource-paths [
+                   "local-repo/core-7.2.100.202105180159.jar"
+                   "cpg-clojure/build/classes/kotlin/main"
+                   ]
   )
--- src/cpg_test/core.clj
+++ src/cpg_test/core.clj
@@ -1,23 +1,53 @@
 (ns cpg-test.core
-    (:import [de.fraunhofer.aisec.cpg TranslationConfiguration TranslationManager]
-             [java.io File]
-             [de.fraunhofer.aisec.cpg.analysis MultiValueEvaluator]
-             [de.fraunhofer.aisec.cpg.helpers SubgraphWalker]
-             [de.fraunhofer.aisec.cpg.graph.statements.expressions CallExpression]))
+    (:require [clojure.string :as str])
+    (:import (de.fraunhofer.aisec.cpg TranslationConfiguration TranslationManager TranslationResult)
+             (de.fraunhofer.aisec.cpg.analysis MultiValueEvaluator)
+             (de.fraunhofer.aisec.cpg.graph Node)
+             (de.fraunhofer.aisec.cpg.graph.statements.expressions CallExpression)
+             (de.fraunhofer.aisec.cpg.helpers SubgraphWalker)
+             (java.io File)
+             (java.util List)
+             (lincks.maximilian.clojure.kotlin MainKt)
+             ))
+
+(def class-for-name "java.lang.Class.forName")
+
+(def load-class-function-name ".loadClass")
+;(defn test []
+;    (Maxi/test))
+
+(defn test2 []
+    (MainKt/main)
+    ;(.sayHi (Human .))
+    )
+
+(defn is-for-name-call?
+    "Checks if the Node is a call to \"java.lang.Class.forName\""
+    [^Node node]
+    (= class-for-name (.getFqn node)))
+
+(defn is-load-class-call?
+    "Checks if the Node is a function named loadClass (TODO: write check if method is called by ClassLoader or Subclasses)"
+    [^Node node]
+    (let [fqn (.getFqn node)]
+        (and
+            (not (nil? fqn))
+            (str/ends-with? fqn load-class-function-name))))
 
 (defn analyse
     "Entry Point to Analyse example file"
-    []
+    [^String file]
     (let [
           config (-> (TranslationConfiguration/builder)
-                     (.sourceLocations (java.util.List/of (File. "C:\\Users\\Maxi\\IdeaProjects\\analyse-project\\src\\root\\main")))
+                     (.sourceLocations (List/of (File. file)))
+                     (.addIncludesToGraph true)
                      (.defaultPasses)
                      (.defaultLanguages)
                      (.build))
           analyzer (-> (TranslationManager/builder)
                        (.config config)
                        (.build))
-          result (.get (.analyze analyzer))
+          ^TranslationResult result (.get (.analyze analyzer))
           applicationNode (-> result
                               (.getAstChildren)
                               (.get 0))
@@ -25,14 +55,20 @@
           nodes (SubgraphWalker/flattenAST applicationNode)
           ]
         (doseq [node nodes]
-            (if (instance? CallExpression node)
+            (if (and
+                    (instance? CallExpression node)
+                    (or
+                        (is-for-name-call? node)
+                        (is-load-class-call? node)))
                 (do
-                    (prn "Node: " node)
                     (prn "Name:" (.getFqn node))
                     (doseq [arg (.getArguments node)]
-                        (prn arg)
                         (prn (.evaluate evaluator arg)))
                     )))
         ))
 
-(defn -main [] (analyse))
+(defn -main []
+    ;(analyse "C:\\Users\\Maxi\\IdeaProjects\\analyse-project\\src\\root\\main")
+    (test2)
+    )
+
