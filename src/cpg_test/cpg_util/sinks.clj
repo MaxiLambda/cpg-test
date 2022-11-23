57318a5a4e8815dab5cd1ff6bf378bd702acf59f
Maxi <maximilian.lincks@gmx.net>
2022-11-23 17:12:40 +0100

    Changed Type Hints from node to callexpression

--- src/cpg_test/cpg_util/sinks.clj
+++ src/cpg_test/cpg_util/sinks.clj
@@ -1,23 +1,23 @@
 (ns cpg-test.cpg-util.sinks
     (:require [clojure.string :as str])
-    (:import (de.fraunhofer.aisec.cpg.graph Node)))
+    (:import (de.fraunhofer.aisec.cpg.graph.statements.expressions CallExpression)))
 
 (def class-for-name "java.lang.Class.forName")
 
 (def load-class-function-name ".loadClass")
 
 (defn is-for-name-call?
-    "Checks if the Node is a call to \"java.lang.Class.forName\""
-    [^Node node]
-    (= class-for-name (.getFqn node)))
+    "Checks if the CallExpression is a call to \"java.lang.Class.forName\""
+    [^CallExpression call-expression]
+    (= class-for-name (.getFqn call-expression)))
 
 (defn is-load-class-call?
     "Checks if the Node is a function named loadClass (TODO: write check if method is called by ClassLoader or Subclasses)"
-    [^Node node]
-    (let [fqn (.getFqn node)]
+    [^CallExpression call-expression]
+    (let [fqn (.getFqn call-expression)]
         (and
             (not (nil? fqn))
             (str/ends-with? fqn load-class-function-name))))
 
-(defn is-sink?
+(defn is-sink? []
     (some-fn is-for-name-call? is-load-class-call?))
